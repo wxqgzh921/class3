26a26fc32421ed3189fd52bff8ca4a28779c07ec
wxqgzh921 <963836594@qq.com>
2019-06-01 01:36:35 +0800

    树的遍历

--- 10.buffer/4.file.js
+++ 10.buffer/4.file.js
@@ -4,6 +4,9 @@
 
 let fs = require('fs')
 //fs.mkdir 也有同步和异步 (创建文件夹,不可跳级创建  创建b必须有a)
+// fs.mkdir("./a/b",(err)=>{
+//     console.log(err);
+// })
 //同步(创建)
 // -----------------------------------------
 // function mkdirSync(path){
@@ -48,12 +51,65 @@ let fs = require('fs')
 // })
 //--------------------------------------------
 
-//同步(删除)
+//删除文件
 // -----------------------------------------
+//fs.unlinkSync('./name.txt') //同步删除文件
 
+//删除文件夹
+// fs.rmdirSync('a')
 
+//读取目录
+//console.log(fs.readdirSync('a')) //看a下面有什么
+//返回文件的状态  isDirectory isFile
+// let stateObj = fs.statSync('a');
+// console.log(stateObj.isDirectory())
 
+//同步删除文件夹 (深度，有儿子先删儿子)
 
-// fs.mkdir("./a/b",(err)=>{
-//     console.log(err);
-// })
+let path = require('path')
+// function rmdirSync(p){
+//     let statObj = fs.statSync(p);
+//     if(statObj.isDirectory()){
+//         let dirs = fs.readdirSync(p); //[]
+//         dirs.map(dir=>{
+//             let current = path.join(p,dir);
+//             rmdirSync(current)
+//         })
+//         fs.rmdirSync(p)
+//     }else{
+//         fs.unlinkSync(p)
+//     }
+// }
+
+// rmdirSync('a')
+
+//广度
+
+function wide(p){
+    let arr = [p];
+    let current;
+    let index = 0
+    while(current = arr[index]){
+        let statObj = fs.statSync(current);
+        if(statObj.isDirectory()){
+            //读取当前目录
+            let dirs = fs.readdirSync(current); //[a,y]
+            //给目录添加path
+            dirs = dirs.map(d=> path.join(current,d)) //[q/a,q/y]
+            //放到数组中
+            arr = [...arr,...dirs];
+            index++;
+        }
+    }
+    for (let i = arr.length-1;i>=0;i--){
+        let current = arr[i]
+        let statObj = fs.statSync(current);
+        if(statObj.isDirectory()){
+            fs.rmdirSync(current)
+        }else{
+            fs.unlinkSync(current)
+        }
+    }
+}
+
+wide('q')
