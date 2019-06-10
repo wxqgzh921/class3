73c8806f379568bb82e74a8280e533d627186e4b
wxqgzh921 <963836594@qq.com>
2019-06-11 00:55:34 +0800

    update promise

--- 2.promise.js/3/3.promise.js
+++ 2.promise.js/3/3.promise.js
@@ -1,6 +1,7 @@
 // promise的链式调用
 
 let fs = require('fs')
+let Promise = require('./promise')
 
 function read(url){
     return new Promise((reslove,reject)=>{
@@ -12,10 +13,22 @@ function read(url){
 }
 
 // 6) 链式调用  promise一旦成功就不能失败  所以promise需每次调用then后返回一个新的promise 可以实现状态的切换
+// read('./name1.txt')
+//     .then((data)=>{
+//         throw new Error('出错了！！')
+//     })
+//     .then((data)=>{
+//         console.log(data)
+//     },err=>{
+//         console.log('err',err)
+//     })
+
 read('./name1.txt')
     .then((data)=>{
-        throw new Error('出错了！')
+        return read(data)
     })
     .then((data)=>{
         console.log(data)
+    },err=>{
+        console.log('err',err)
     })
--- 2.promise.js/3/promise.js
+++ 2.promise.js/3/promise.js
@@ -1,3 +1,11 @@
+function reslovePromise(promise2,x,reslove,reject){
+    // console.log(promise2)
+    // 判断x的类型 如果是常量 则promise成功  如果是promise 则promise的结果
+    if(typeof(x) === ){
+        reslove(x)
+    }
+}
+
 class Promise{
     constructor(executor){
         this.status = 'pedding';
@@ -28,20 +36,53 @@ class Promise{
     }
     
     then(onfulfilled,onrejected){
-        if(this.status == 'success'){
-            onfulfilled(this.value)
-        }
-        if(this.status == 'error'){
-            onrejected(this.reason)
-        }
-        if(this.status == 'pedding'){
-            this.resloveCallbacks.push(()=>{
-                onfulfilled(this.value)
-            })
-            this.rejectCallbacks.push(()=>{
-                onrejected(this.reason)
-            })
-        }
+        let promise2;
+        // 调用then后必须返回一个新的promise;;
+        promise2 = new Promise((reslove,reject)=>{
+            if(this.status == 'success'){
+                setTimeout(function(){
+                    try {
+                        let x = onfulfilled(this.value)
+                        reslovePromise(promise2,x,reslove,reject)
+                    } catch (error) {
+                        reject(error)
+                    }
+                },0)
+            }
+            if(this.status == 'error'){
+                setTimeout(function(){
+                    try {
+                        let x = onrejected(this.reason)
+                        reslovePromise(promise2,x,reslove,reject)
+                    } catch (error) {
+                        reject(error)
+                    }
+                },0)
+            }
+            if(this.status == 'pedding'){
+                this.resloveCallbacks.push(()=>{
+                    setTimeout(function(){
+                        try {
+                            let x = onfulfilled(this.value)
+                            reslovePromise(promise2,x,reslove,reject)
+                        } catch (error) {
+                            reject(error)
+                        }
+                    },0)
+                })
+                this.rejectCallbacks.push(()=>{
+                    setTimeout(function(){
+                        try {
+                            let x = onrejected(this.reason)
+                            reslovePromise(promise2,x,reslove,reject)
+                        } catch (error) {
+                            reject(error)
+                        }
+                    },0)
+                })
+            }
+        })
+        return promise2;
     }
 }
 module.exports =  Promise;
