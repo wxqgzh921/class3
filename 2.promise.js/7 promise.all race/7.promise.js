ef2ced21b19701cd05dce20fbb6d3f059f1eb21a
wxqgzh921 <963836594@qq.com>
2019-06-14 00:35:56 +0800

    update promise all

--- 2.promise.js/7 promise.all race/7.promise.js
+++ 2.promise.js/7 promise.all race/7.promise.js
@@ -1,31 +1,46 @@
 
-let fs = require('fs')
-// let Promise = require('./promise');
-// let bulebird = require('bluebird');
+// let fs = require('fs')
+let fs = require('mz/fs')
 
-
-// let read = bulebird.promisify(fs.readFile);
-
-// read('name1.txt','utf8').then(data=>{
-//     console.log(data)
-// })
-
-// pormisify promise化   node中已经借鉴了 所有的异步方法参数第一个都是error
-// function promisify(fn){
-//     return function(){
-//         return new Promise((resolve,reject)=>{
-//             fn(...arguments,(err,data)=>{
-//                 if(err) reject(err)
-//                 resolve(data)
-//             })
-//         })
-//     }
-// }
-
-// let read = promisify(fs.readFile);
-
-// read('name1.txt','utf8').then(data=>{
-//     console.log(data)
-// })
 
 // mz 它里面把所有的node模块 都进行了包装 包装成promise 
+
+// fs.readFile('name2.txt','utf8').then((data)=>{
+//     console.log(data)
+// })
+
+// promise all 
+
+Promise.all = function(values){
+    return new Promise((resolve,reject)=>{
+        let results = []; // 放结果的数组
+        let i = 0;
+        let handleArr = (value,index)=>{
+            results[index] = value;
+            if( ++i === values.length){
+                resolve(results)
+            }
+        }
+        for(let i = 0 ; i < values.length; i++ ){
+            let current = values[i];
+            if( typeof current === "function" || (typeof current === "Object" && current !== null)){
+                if( typeof current.then === "function"){
+                    current.then(data=>{
+                        handleArr(data,i);
+                    },e=>{
+                        reject(e);
+                    })
+                }else{
+                    handleArr(current,i)
+                }
+            }else{
+                handleArr(current,i)
+            }
+        }
+    })
+}
+
+Promise.all([fs.readFile('name1.txt','utf8'),fs.readFile('name2.txt','utf8'),1,2,3])
+    .then(data=>{
+        console.log(data)
+    })
